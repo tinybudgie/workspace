400dc4b57392098eeb45feeb6814a55b7fcae2ef
temz <artem.rusanov.02@gmail.com>
2022-12-16 23:11:21 +0700

    chore(docker): add database url parser

--- docker/dev/docker-compose-up.sh
+++ docker/dev/docker-compose-up.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 export CURRENT_UID=$(id -u):$(id -g)
 
+# extract the protocol
+proto="`echo $DATABASE_URL | grep '://' | sed -e's,^\(.*://\).*,\1,g'`"
+# remove the protocol
+url=`echo $DATABASE_URL | sed -e s,$proto,,g`
+
+# extract the user and password (if any)
+userpass="`echo $url | grep @ | cut -d@ -f1`"
+pass=`echo $userpass | grep : | cut -d: -f2`
+if [ -n "$pass" ]; then
+    user=`echo $userpass | grep : | cut -d: -f1`
+else
+    user=$userpass
+fi
+
+database=`echo $url | sed -e s,$userpass@$hostport/,,g | cut -d? -f1`
+
+export POSTGRES_USER=$user
+export POSTGRES_PASSWORD=$pass
+export POSTGRES_DB=$database
+
 docker volume create --name=nx-postgres-volume --label=nx-postgres-volume
 
 # Start only database
@@ -19,4 +39,4 @@ npx nx build
 npx prisma migrate deploy
 
 # Start all services
-docker-compose -f ./docker/dev/docker-compose.yml --compatibility up -d
+docker-compose -f ./docker/dev/docker-compose.yml --compatibility up
