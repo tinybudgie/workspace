8767c9517d05824cc4ea0e8d7a746e60cbee784f
temz <artem.rusanov.02@gmail.com>
2023-02-05 16:16:34 +0700

    feat: add docusaurus app

---
+++ apps/docs/docusaurus.config.js
@@ -0,0 +1,64 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'NX Docs',
+  tagline: 'NX useful libs and tools',
+  url: 'https://temarusanov.github.io',
+  baseUrl: '/nx/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'temarusanov', // Usually your GitHub org/user name.
+  projectName: 'nx', // Usually your repo name.
+  trailingSlash: false,
+  presets: [
+    ['@docusaurus/preset-classic',
+    {
+      docs: {
+        routeBasePath: '/', // Serve the docs at the site's root
+        /* other docs plugin options */
+      },
+      blog: false, // Optional: disable the blog plugin
+      // ...
+    },]
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'NX Docs',
+        logo: {
+          alt: 'NX docs logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'home-doc',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/temarusanov/nx',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()}. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
