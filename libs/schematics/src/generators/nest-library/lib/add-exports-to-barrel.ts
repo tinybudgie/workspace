100b13f00d7480d8d14e84c5385f95288817514d
temz <artem.rusanov.02@gmail.com>
2023-03-03 12:41:09 +0700

    feat: add schematics

---
+++ libs/schematics/src/generators/nest-library/lib/add-exports-to-barrel.ts
@@ -0,0 +1,57 @@
+import type { Tree } from '@nrwl/devkit';
+import {
+  addGlobal,
+  removeChange,
+} from '@nrwl/workspace/src/utilities/ast-utils';
+import type { NormalizedOptions } from '../schema';
+import { ensureTypescript } from '@nrwl/js/src/utils/typescript/ensure-typescript';
+
+let tsModule: typeof import('typescript');
+
+export function addExportsToBarrelFile(
+  tree: Tree,
+  options: NormalizedOptions
+): void {
+  if (!tsModule) {
+    tsModule = ensureTypescript();
+  }
+  const indexPath = `${options.projectRoot}/src/index.ts`;
+  const indexContent = tree.read(indexPath, 'utf-8');
+  let sourceFile = tsModule.createSourceFile(
+    indexPath,
+    indexContent,
+    tsModule.ScriptTarget.Latest,
+    true
+  );
+
+  sourceFile = removeChange(
+    tree,
+    sourceFile,
+    indexPath,
+    0,
+    `export * from './lib/${options.fileName}';`
+  );
+  sourceFile = addGlobal(
+    tree,
+    sourceFile,
+    indexPath,
+    `export * from './lib/${options.fileName}.module';`
+  );
+
+  if (options.service) {
+    sourceFile = addGlobal(
+      tree,
+      sourceFile,
+      indexPath,
+      `export * from './lib/${options.fileName}.service';`
+    );
+  }
+  if (options.controller) {
+    sourceFile = addGlobal(
+      tree,
+      sourceFile,
+      indexPath,
+      `export * from './lib/${options.fileName}.controller';`
+    );
+  }
+}
