6c30c0617cc8fa1924ceac5e873317527a534e3d
temz <artem.rusanov.02@gmail.com>
2023-03-04 13:43:48 +0700

    chore: remove prisma client

--- libs/core/prisma-client/src/lib/prisma-client.config.ts
+++
@@ -1,7 +0,0 @@
-export const PRISMA_CLIENT_CONFIG = Symbol('PRISMA_CLIENT_CONFIG')
-
-export interface PrismaClientConfig {
-    databaseUrl: string
-    logging: 'all_queries' | 'long_queries'
-    maxQueryExecutionTime: number
-}
