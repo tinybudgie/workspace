1dd56170eaf84f9dd532bdfb84630840b2585078
temz <artem.rusanov.02@gmail.com>
2023-06-15 18:20:01 +0700

    fix: add webpack dep

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -9,19 +9,19 @@ import * as env from 'env-var'
 @Module({
     imports: [
         HealthChecksModule,
+        EventloopFrozenDetectorModule.forRoot({
+            delay: 3000,
+        }),
         GraphQLModule.forRoot<YogaDriverConfig>({
             driver: YogaDriver,
             autoSchemaFile: 'schema.graphql',
         }),
         SamplePrismaClientModule.forRoot({
-            databaseUrl: env.get('DATABASE_URL').required().asString(),
+            databaseUrl: env.get('SAMPLE_DATABASE_URL').required().asString(),
             logging: 'long_queries',
             maxQueryExecutionTime: 5000,
         }),
         SampleModule.forRoot({}),
-        EventloopFrozenDetectorModule.forRoot({
-            delay: 3000,
-        }),
     ],
     controllers: [],
     providers: [],
