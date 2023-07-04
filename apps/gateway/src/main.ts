8e2f9e2c7f5a7fa958428c83e491110dd5ff1125
temz <artem.rusanov.02@gmail.com>
2023-07-04 22:33:50 +0700

    lint: format files

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -58,6 +58,7 @@ function exitHandler(options, exitCode) {
 }
 
 // Copy output to mermaid.live
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function buildDIGraph(app: INestApplication) {
     const tree = SpelunkerModule.explore(app)
     const root = SpelunkerModule.graph(tree)
