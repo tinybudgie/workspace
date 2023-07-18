cf31c4de10ffbc18f361e95cc0070c8cfbc5e5e3
temz <artem.rusanov.02@gmail.com>
2023-07-18 19:51:48 +0700

    feat: moved schematics to external

--- libs/external/schematics/jest.config.ts
+++ libs/external/schematics/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
     displayName: 'schematics',
-    preset: '../../jest.preset.js',
+    preset: '../../../jest.preset.js',
     globals: {
         'ts-jest': {
             tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -11,5 +11,5 @@ export default {
         '^.+\\.[tj]s$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageDirectory: '../../coverage/libs/schematics',
+    coverageDirectory: '../../../coverage/libs/external/schematics',
 }
